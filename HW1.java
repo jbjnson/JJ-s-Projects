0bae70cfac14d4f7d85d8938178ff7a5a75546b6
jbjnson <77993874+jbjnson@users.noreply.github.com>
2021-02-01 15:36:11 -0700

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,43 @@
+// HW1 Assignment
+// Author: Justin Johnson
+// Date: 1/29/21
+// Class: CS1050 Sec#1
+// Email: jjohn395@msudenver.edu
+
+
+
+public class HW1 {
+
+
+   public static void main (String[] args) {
+   
+   //declare variables
+   
+   float a = 3.14f;
+   float b = 2.5f;
+   
+   //print out what's required
+   
+      System.out.println("Let's learn together!");
+   
+      System.out.println("My Homework 1");
+   
+      System.out.println("Johnson, " + "" + "Justin");
+   
+      System.out.println("Computer Science or " + "Undeclared");
+   
+      System.out.println(1234 * 2);
+   
+      String s = String.format("%.2f", (a * b)); // cutting off "floating zeros using modulo operator," 
+                              //source: (https://farenda.com/java/java-format-double-2-decimal-places/)
+   
+      System.out.println("The product of 2 decimals is " + s);
+   
+      System.out.println("CS1050 \\ Java"); // forward slash as an escape sequence 
+   
+      System.out.println("I am taking this class because I want a good foundation for computer programming");
+      System.out.println("with hopes of applying newfound skills to a cyber-security related field."); 
+   
+   }  //end main method
+
+} // end HW 1
